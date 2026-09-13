f19b05a183f6699f0e6cfc07c95eabfe3f5cea7a
maria7674430 <maria.7674430@aluno.mg.gov.br>
2026-09-13 12:54:22 -0300

    Conexão supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://quuynojwktqgiuimwyaz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_hXpADatHVfEdvxcq8xajCg_qEa0cThs";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
